c52aff4074cb1f594402635509e99340a4778cea
omegadeep10 <omegadeep10@outlook.com>
2016-03-19 12:36:43 -0400

    Add list.js search integration

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var options = {
+    valueNames: ["crn", "ptrm", "subject", "number", "section", "campus", "title", "seats", "hours", "begin", "end", "building", "room", "days", "instructor"]
+}
+
+var userList = new List('course-data', options);
