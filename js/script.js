7f00804903597730c39294482959cc01b6179dfe
omegadeep10 <omegadeep10@outlook.com>
2016-03-22 17:10:39 -0400

    Add pagination and initial header design

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var options = {
-    valueNames: ["crn", "ptrm", "subject", "number", "section", "campus", "title", "seats", "hours", "begin", "end", "building", "room", "days", "instructor"]
-}
+    valueNames: ["crn", "ptrm", "subject", "number", "section", "campus", "title", "seats", "hours", "begin", "end", "building", "room", "days", "instructor"],
+    page: 50,
+    plugins: [ ListPagination({
+        paginationClass: "pagination"
+    }) ]
+};
 
 var userList = new List('course-data', options);
